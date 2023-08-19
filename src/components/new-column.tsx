f0dee42db80d6d4ba4b51dc55de6c181effe6929
Alex Dmitriev <alexey.dmitriev6238@gmail.com>
2023-08-19 16:54:30 -0700

    Rename new column to new list

--- src/components/new-column.tsx
+++ src/components/new-column.tsx
@@ -9,9 +9,9 @@ export default function NewColumn({ board }: { board: BoardWithLists }) {
     <div className="pr-6 ">
       <div className="flex h-full w-[17.5rem] flex-col">
         {/* List Header */}
-        <div className="flex items-center invisible gap-3">
+        <div className="invisible flex items-center gap-3">
           <div className="h-[15px] w-[15px] rounded-full bg-slate-600" />
-          <h2 className="uppercase text-heading-s text-medium-grey">
+          <h2 className="text-heading-s uppercase text-medium-grey">
             New Column
           </h2>
         </div>
@@ -22,7 +22,7 @@ export default function NewColumn({ board }: { board: BoardWithLists }) {
           }}
           className="mt-6 grow rounded-md bg-gradient-to-b from-[#E9EFFA] to-[#E9EFFA]/50 text-heading-xl text-medium-grey hover:text-main-purple dark:from-[#2B2C37]/25 dark:to-[#2B2C37]/10"
         >
-          New column
+          + New List
         </button>
       </div>
 
